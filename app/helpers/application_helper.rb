2def63484ed789370c9e86950753969388eec41c
Dainel Vera <arubinofaux@gmail.com>
2016-09-11 16:47:57 -0400

    indicate auth failure and success in the UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def growl(type)
+    case type
+    when 'alert'
+      return 'warning'
+    when 'notice'
+      return 'notice'
+    else
+      return 'default'
+    end
+  end
 end
